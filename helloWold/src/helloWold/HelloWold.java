d09970cff1b2fa060ec6365cbc3f1d0b429f2f09
jjm- <skull494@naver.com>
2016-11-09 18:36:04 +0900

    add a print line.

--- helloWold/src/helloWold/HelloWold.java
+++ helloWold/src/helloWold/HelloWold.java
@@ -5,6 +5,7 @@ public class HelloWold {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Wold");
+		System.out.println("Hello Eveyare");
 	}
 
 }
