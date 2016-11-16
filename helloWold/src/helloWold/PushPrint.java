647609636d81d3f7b0d3bcbdaff45151b2807084
jjm494 <skull494@naver.com>
2016-11-16 18:14:45 +0900

    PushPrintln.java modifed on GitHub

--- helloWold/src/helloWold/PushPrint.java
+++ helloWold/src/helloWold/PushPrint.java
@@ -5,6 +5,7 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add Class File Push");
+		System.out.println("wold with 365");
 	}
 
 }
